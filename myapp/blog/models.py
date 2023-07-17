b13a2fc805cac48ee0615441977c2dfb12eb73a0
Seungwon Park <psw70408@gmail.com>
2023-07-17 12:03:13 +0900

    first commit

---
+++ myapp/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    writer = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
